731bc53f06ab14816383149e6154d1e61de6a78c
ElPrado <75703856+Melonpan37@users.noreply.github.com>
2022-12-01 10:03:06 +0100

    Update main.js

--- renderer_02/main.js
+++ renderer_02/main.js
@@ -215,6 +215,7 @@ Renderer.drawObject = function (gl, obj, shader, fillColor) {
 
   gl.drawElements(gl.TRIANGLES, obj.triangleIndices.length, gl.UNSIGNED_SHORT, 0);
 
+  if(!obj.hasOwnProperty("indexBufferEdges")) return;
   //draw edges
   gl.disable(gl.POLYGON_OFFSET_FILL);
   gl.uniform4fv(this.shader.uColorLocation, new Float32Array([0.0, 0.0, 0.0, 1.0]));
